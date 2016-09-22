f628a151a009faa980ff6490a7201c831f693cdc
Thijs Janzen <janzen@evolbio.mpg.de>
2016-09-22 18:37:25 +0200

    lintR bot improvements

--- R/ESF_function.R
+++ R/ESF_function.R
@@ -1,8 +1,7 @@
 logLikelihood.ESF <- function(theta, m, abund) {
-  if(theta < 1 || 
-     m > (1-.Machine$double.eps) ||
-     m <= 0
-     ) {
+  if (theta < 1 ||
+     m > (1 - .Machine$double.eps) ||
+     m <= 0) {
      return(-Inf)
   }
 
@@ -12,18 +11,18 @@ logLikelihood.ESF <- function(theta, m, abund) {
 
   kda <- calcKDA(abund)  #confirmed in PARI
 
-  sumkda <- calc_sum_kda(S, J, I, theta, kda)  
- 
+  sumkda <- calc_sum_kda(S, J, I, theta, kda)
+
   x <- c(table(abund))
   freq_x <- c()
-  for(i in seq_along(x)) freq_x[i] <- x[[i]]
-  prefactor1 <- -( sum(log(abund)) + sum(lgamma(1 + freq_x)) )
-  
+  for (i in seq_along(x)) freq_x[i] <- x[[i]]
+  prefactor1 <- -1 * ( sum(log(abund)) + sum(lgamma(1 + freq_x)))
+
   #J!/[prod(n1)prod(Sx!)]  #confirmed in PARI
-  factor1 <- lgamma(J+1) + prefactor1  
-  
+  factor1 <- lgamma(J+1) + prefactor1
+
   factor2 <- S*log(theta)   -  (lgamma(I + J) - lgamma(I))
-  
+
   ll <- factor1 + factor2 + sumkda
   return(ll)
 }
@@ -32,20 +31,20 @@ esf_local <- function(v, abund, prefactor, kda) {
   theta <- v[1]
   m <- v[2]
 
-  if(theta < 1 || 
-     m <= 0 || 
-     m > (1-.Machine$double.eps)) {
+  if (theta < 1 ||
+     m <= 0 ||
+     m > (1 - .Machine$double.eps)) {
     return(-Inf)
   }
- 
+
   J <- sum(abund)
   S <- length(abund)
-  I <- m * (J-1) / (1 - m)
+  I <- m * (J - 1) / (1 - m)
 
   sumkda <- calc_sum_kda(S, J, I, theta, kda)
- 
-  factor2 <- S*log(theta)   -  (lgamma(I + J) - lgamma(I))
-  
+
+  factor2 <- S * log(theta)   -  (lgamma(I + J) - lgamma(I))
+
   ll <- prefactor + factor2 + sumkda
   return(ll)
 }
@@ -71,17 +70,16 @@ maxLikelihood.ESF <- function(init_vals, abund, verbose=TRUE) {
   kda <- calcKDA(abund)  #confirmed in PARI
 
   J <- sum(abund)
-  S <- length(abund)
   x <- c( table(abund))
   freq_x <- c()
   for (i in seq_along(x)) freq_x[i] <- x[[i]]
-  prefactor <- lgamma(J+1) -( sum(log(abund)) + sum(lgamma(1 + freq_x)) )
-   
+  prefactor <- lgamma(J+1) - ( sum(log(abund)) + sum(lgamma(1 + freq_x)) )
+
   g <- function(x) {
 	  out <- -1 * esf_local(x, abund, prefactor, kda)
 		return(out)
-  }  
-  
+  }
+
   optimum <- simplex(init_vals, g, verbose)
   return(optimum)
 }
--- R/expected.R
+++ R/expected.R
@@ -1,8 +1,8 @@
-prestonSort <- function(A) {
+prestonsort <- function(A) {
   output <- rep(0,13)
-  
-  for(k in 1:length(output)) {
-    start <- 2^(k-1)
+
+  for (k in seq_along(output)) {
+    start <- 2^(k - 1)
     end <- -1 + 2^(k)
     if(end > length(A)) {
       end <- length(A)
@@ -16,52 +16,51 @@ prestonSort <- function(A) {
   return(output)
 }
 
-
 expected.SAD <- function(theta, m, J) {
-  if(theta < 1) {
+  if (theta < 1) {
     stop("expected.SAD: ",
          "theta can not be below one")
   }
-  if(m < 0) {
+  if (m < 0) {
     stop("expected.SAD: ",
          "m can not be below zero")
   }
-  if(m > 1) {
+  if (m > 1) {
     stop("expected.SAD: ",
          "m can not be above 1")
   }
-  if(J < 0) {
+  if (J < 0) {
     stop("expected.SAD: ",
          "J can not be below zero")
   }
-  
-  I = (J-1)* m / (1-m)
+
+  I = (J - 1) * m / (1 - m)
   aux <- pm_sad(theta, I, J)
-  SAD <- prestonSort(aux)
-  return(SAD)
+  sad <- prestonsort(aux)
+  return(sad)
 }
 
 expected.SAD.Guilds <- function(theta, alpha_x, alpha_y,
                                 J, n_replicates = 100) {
-  meanX <- rep(0, J)
-  meanY <- rep(0, J)
+  meanx <- rep(0, J)
+  meany <- rep(0, J)
 
-  for (r in 1:n_replicates) {
+  for (r in seq_len(n_replicates)) {
 		M <- draw_local(theta, alpha_x, alpha_y, J)
-		for (m in 1:length(M$guildX)) {
-			meanX[m] <- meanX[m] + M$guildX[m]
+		for (m in seq_along(M$guildX)) {
+			meanx[m] <- meanx[m] + M$guildX[m]
 		}
-		for (m in 1:length(M$guildY)) {
-			meanY[m] <- meanY[m] + M$guildY[m]
+		for (m in seq_along(M$guildY)) {
+			meany[m] <- meany[m] + M$guildY[m]
 		}
   }
-  meanX <- meanX / n_replicates
-  meanY <- meanY / n_replicates
+  meanx <- meanx / n_replicates
+  meany <- meany / n_replicates
 
-  gX <- prestonSort(meanX)
-  gY <- prestonSort(meanY)
+  gx <- prestonsort(meanx)
+  gy <- prestonsort(meany)
   
-  output <- list( guildX = gX, guildY = gY)
+  output <- list( guildx = gX, guildY = gy)
   return(output)
 }
 
@@ -71,24 +70,24 @@ expected.SAD.Guilds.Conditional <- function(theta,
                                             Jx,
                                             Jy,
                                             n_replicates = 100) {
-  meanX <- rep(0,Jx)
-  meanY <- rep(0,Jy)
+  meanx <- rep(0,Jx)
+  meany <- rep(0,Jy)
 
-  for (r in 1:n_replicates) {
+  for (r in seq_len(n_replicates)) {
 		M <- draw_local_cond(theta, alpha_x, alpha_y, Jx, Jy)
-		for (m in 1:length(M$guildX)) {
-			meanX[m] <- meanX[m] + M$guildX[m]
+		for (m in seq_along(M$guildX)) {
+			meanx[m] <- meanx[m] + M$guildX[m]
 		}
-		for (m in 1:length(M$guildY)) {
-			meanY[m] <- meanY[m] + M$guildY[m]
+		for (m in seq_along(M$guildY)) {
+			meany[m] <- meany[m] + M$guildY[m]
 		}
   }
-  meanX <- meanX / n_replicates
-  meanY <- meanY / n_replicates
+  meanx <- meanx / n_replicates
+  meany <- meany / n_replicates
 
-  gX <- prestonSort(meanX)
-  gY <- prestonSort(meanY)
+  gx <- prestonsort(meanx)
+  gy <- prestonsort(meany)
   
-  output <- list( guildX = gX, guildY = gY)
+  output <- list( guildX = gx, guildY = gy)
   return(output)
 }
