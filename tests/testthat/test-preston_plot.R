7d24edf82569905c1ddfd31c6f4a755c9033470c
Thijs Janzen <janzen@evolbio.mpg.de>
2016-09-23 00:03:14 +0200

    update NAMESPACE

--- tests/testthat/test-preston_plot.R
+++ tests/testthat/test-preston_plot.R
@@ -3,11 +3,11 @@ context("preston_plot")
 test_that("preston_plot use", {
   theta = 100
   m = 0.1
-  I = m * (J - 1) / (1 - m)
   J = 10000
+  I = m * (J - 1) / (1 - m)
   
   abund <- generate.ESF(theta, I, J)
-  par(mfrow = c(1,2))
+  par(mfrow = c(1, 2))
   preston_plot(abund)
   abund.expect <- expected.SAD(theta, m, J)
   preston_plot(abund, abund.expect)
