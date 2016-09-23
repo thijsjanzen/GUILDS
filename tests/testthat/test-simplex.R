4527250fc96c95e53cfc85fef2de7dcbd3f64c30
Thijs Janzen <janzen@evolbio.mpg.de>
2016-09-23 09:33:45 +0200

    added simplex test

---
+++ tests/testthat/test-simplex.R
@@ -0,0 +1,22 @@
+context("simplex")
+
+test_that("simplex use", {
+  
+  a <- 3
+  xvals <- 1:10
+  yvals <- a * xvals + rnorm(10)
+  
+  evalfunc <- function(x) {
+    sim_y <- xvals * x
+    diff <- yvals - sim_y
+    ll <- sum(dnorm(diff, mean = 0, sd = 1, log =TRUE))
+    return(-ll)
+  }
+  
+  v <- simplex(initpars=c(2), evalfunc, 
+               verbose = FALSE,
+               maxiter = 1000)
+  v <- simplex(initpars=c(2), evalfunc, 
+               verbose = TRUE,
+               maxiter = 10)
+})
